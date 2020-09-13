8ac96399580c6516a7d577e2b8018424c8b91b76
Gautam Maurya <44540064+gautam-maurya@users.noreply.github.com>
2020-09-13 15:32:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-	res.render('m');
+	res.render('index');
 })
 
 app.listen(port, function() {
