9f70669b616a3dd0b6861441927aa09819a965c3
Gautam Maurya <44540064+gautam-maurya@users.noreply.github.com>
2020-09-19 08:09:15 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <h1>City Centre</h1>
     <h2>Developers</h2>
     <p style="font-size:20px">Gautam Maurya : +919004820117</p>
-    <p style="font-size:20px">Harsh Jalan : +919920023829</p>
+    <p style="font-size:20px">Canute Corda : +9198992703327</p>
     <a href="lindex.php?sort=basic">&lt;&lt;&lt;&lt;Home</a></li>
 </body>
 </html>
